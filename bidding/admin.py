9e424628d04fbbed91969fb0b91db947440abb40
SORRENTI Michael <DXJ@dsone.3ds.com>
2017-10-19 16:47:15 -0400

    update email generation structure; clear bids action

--- bidding/admin.py
+++ bidding/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin, messages
 from django.conf import settings
+import os
 
 # Import email capabilities from sendgrid
 import sendgrid
@@ -31,7 +32,7 @@ class AuctionItemAdmin(admin.ModelAdmin):
     #inlines = [BidInline]
     list_filter = ('current_bidder',)
     
-    actions = ['close_bidding', 'open_bidding', 'email_winners']
+    actions = ['close_bidding', 'open_bidding', 'email_winners', 'clear_bids']
     
     def close_bidding(self, request, queryset):
         queryset.update(bidding_open = False)
@@ -40,6 +41,12 @@ class AuctionItemAdmin(admin.ModelAdmin):
     def open_bidding(self, request, queryset):
         queryset.update(bidding_open = True)
     open_bidding.short_description = "Open bidding on selected items"
+    
+    #this function clears bids in the current_bid & current_bidder fields of the AuctionItem
+    #model; it does not affect the Bid model
+    def clear_bids(self, request, queryset):
+        queryset.update(current_bid = 0)
+        queryset.update(current_bidder = '')
 
     #email highest bidders on selected items with a link to payment
     def email_winners(self, request, queryset): 
@@ -49,11 +56,6 @@ class AuctionItemAdmin(admin.ModelAdmin):
         #module for a possible pattern
         
         successful_emails = 0
-        
-        #raise a notification for any selected bidders who do not have email addresses
-        for selected_item in queryset:
-            if not selected_item.current_bidder.user.email:
-                self.message_user(request, '%s does not have an email address' % selected_item.current_bidder.user, messages.WARNING)
                 
         for bidder in Bidder.objects.all():
             #for each bidder in the system, construct the list of selected items on which they 
@@ -63,30 +65,31 @@ class AuctionItemAdmin(admin.ModelAdmin):
             #make sure the bidder has both winning bids and an email address
             if winning_items and bidder.user.email:
                 total_bid = 0
-                #Open the HTML table tag & set heading
-                item_table_html = "<table cellpadding=\"5px\" border=\"1px\"><tr><th>Item</th><th>Bid</th></tr>"
+                #String of HTML to be inserted into the email template table
+                item_table_html = ""
                 for item in winning_items:
                     total_bid += item.current_bid
                     #Create new HTML table rows for each item
                     item_table_html += "<tr><td>" + item.name + " </td><td>$" + str(item.current_bid) + "</td></tr>"
-                #Add summary row and close the HTML table    
-                item_table_html += "<tr style='font-weight: bold;'><td style='text-align: right'>Total: </td><td>$" + str(total_bid) + "</td></tr></table>"
-                
-                #self.message_user(request, item_table_html)
-        
+                                
                 #send email message via sendgrid
-                sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
+                sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
                 from_email = Email("test@example.com")
                 subject = "Will be overridden by template"
                 to_email = Email(bidder.user.email)
-                content = Content("text/html", item_table_html)
+                content = Content("text/html", "Will be overridden by template")
                 mail = Mail(from_email, subject, to_email, content)
                 mail.personalizations[0].add_substitution(Substitution("-firstname-", bidder.user.first_name))
+                mail.personalizations[0].add_substitution(Substitution("<tr><td>TO-BE-REPLACED</td></tr>", item_table_html))
+                mail.personalizations[0].add_substitution(Substitution("-totalbid-", str(total_bid)))
                 mail.template_id = "fd12b8e2-24bf-470c-b780-4d4ccd4e94b9"
                 response = sg.client.mail.send.post(request_body=mail.get())
                 if response.status_code==202:
                     successful_emails += 1
                 
+            #If a winning bidder does not have an email address, raise a notification
+            elif winning_items and not bidder.user.email:
+                self.message_user(request, '%s does not have an email address' % bidder.user, messages.WARNING)
                                       
         self.message_user(request, '%s emails sent successfully' % successful_emails)
         
